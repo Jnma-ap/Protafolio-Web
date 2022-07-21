bdf2658c5fdbdce3b1f4fa4da7f12940c9c219be
Juanma-Alvarado <juanmanuel3alvarado@gmail.com>
2022-07-21 09:51:31 -0500

    Modificación Final

--- Proyectos/P2/main.js
+++ Proyectos/P2/main.js
@@ -1,52 +1,82 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const clock = new BouncyBlockClock(".clock");
+});
 
- const articles = new Map([
-    ["6392322", { tx: "-90%", tz: "-70vmin", ry: "60deg" }],
-    ["1761279", { tz: "-110vmin" }],
-    ["1679772", { tx: "90%", tz: "-70vmin", ry: "-60deg" }]
-  ]);
-  
-  window.addEventListener("load", () => {
-    const main = document.querySelector("main");
-  
-    for (const [id, { tx, tz, ry }] of articles.entries()) {
-      main.appendChild(makeArticleElement(id, tx, tz, ry));
-    }
-  
-    document.addEventListener("click", ({ target }) => {
-      const targetId = target.closest("article")?.id;
-      let [itx, itz, iry] = [0, 0, 0]; // inversed transformation to apply to the main element
-  
-      if (targetId && main.dataset.focus !== targetId) {
-        // zoom in
-        const { tx, tz, ry } = articles.get(targetId) || {};
-        [itx, itz, iry] = [tx, tz, ry].map(inverseTransformation);
-        main.setAttribute("data-focus", targetId);
-      } else {
-        // zoom out
-        main.removeAttribute("data-focus");
-      }
-      main.style.transform = `rotateY(${iry}) translate3d(${itx}, 0, ${itz})`;
+class BouncyBlockClock {
+  constructor(qs) {
+    this.el = document.querySelector(qs);
+    this.time = { a: [], b: [] };
+    this.rollClass = "clock__block--bounce";
+    this.digitsTimeout = null;
+    this.rollTimeout = null;
+    this.mod = 0 * 60 * 1000;
+
+    this.loop();
+  }
+  animateDigits() {
+    const groups = this.el.querySelectorAll("[data-time-group]");
+
+    Array.from(groups).forEach((group, i) => {
+      const { a, b } = this.time;
+
+      if (a[i] !== b[i]) group.classList.add(this.rollClass);
     });
-  });
-  
-  // e.g. turn "90%" into "-90%" or "-10vmin" into "10vmin"
-  function inverseTransformation(transform) {
-    if (!transform) return 0;
-    const [_, value, unit] = transform.match(/(-?\d+)(.*)/);
-    return `${-Number(value)}${unit}`;
+
+    clearTimeout(this.rollTimeout);
+    this.rollTimeout = setTimeout(this.removeAnimations.bind(this), 900);
   }
-  
-  function makeArticleElement(id, tx = 0, tz = 0, ry = 0) {
-    const img = document.createElement("img");
-    img.src = `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
-  
-    const button = document.createElement("button");
-    button.appendChild(img);
-  
-    const element = document.createElement("article");
-    element.id = id;
-    element.style.transform = `translate3d(${tx}, 0, ${tz}) rotateY(${ry})`;
-    element.appendChild(button);
-    return element;
+  displayTime() {
+    // screen reader time
+    const timeDigits = [...this.time.b];
+    const ap = timeDigits.pop();
+
+    this.el.ariaLabel = `${timeDigits.join(":")} ${ap}`;
+
+    // displayed time
+    Object.keys(this.time).forEach((letter) => {
+      const letterEls = this.el.querySelectorAll(`[data-time="${letter}"]`);
+
+      Array.from(letterEls).forEach((el, i) => {
+        el.textContent = this.time[letter][i];
+      });
+    });
   }
-  
+  loop() {
+    this.updateTime();
+    this.displayTime();
+    this.animateDigits();
+    this.tick();
+  }
+  removeAnimations() {
+    const groups = this.el.querySelectorAll("[data-time-group]");
+
+    Array.from(groups).forEach((group) => {
+      group.classList.remove(this.rollClass);
+    });
+  }
+  tick() {
+    clearTimeout(this.digitsTimeout);
+    this.digitsTimeout = setTimeout(this.loop.bind(this), 1e3);
+  }
+  updateTime() {
+    const rawDate = new Date();
+    const date = new Date(Math.ceil(rawDate.getTime() / 1e3) * 1e3 + this.mod);
+    let h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    const ap = h < 12 ? "AM" : "PM";
+
+    if (h === 0) h = 12;
+    if (h > 12) h -= 12;
+
+    this.time.a = [...this.time.b];
+    this.time.b = [
+      h < 10 ? `0${h}` : `${h}`,
+      m < 10 ? `0${m}` : `${m}`,
+      s < 10 ? `0${s}` : `${s}`,
+      ap,
+    ];
+
+    if (!this.time.a.length) this.time.a = [...this.time.b];
+  }
+}
